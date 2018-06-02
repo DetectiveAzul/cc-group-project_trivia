0eb7d4bddc1cf2347bd33022da09661ce3cb4055
Jaime Lopez <detec.azul@gmail.com>
2018-06-02 12:34:18 +0100

    All the routes for highScorerouter all done and tested

--- server/routers/high_scores_router.js
+++ server/routers/high_scores_router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ObjectID = require('mongodb').ObjectID;
 
+
 const highScoresRouter = function(highScoresCollection) {
   //index
   router.get('/', (req, res) => {
@@ -18,6 +19,29 @@ const highScoresRouter = function(highScoresCollection) {
       .find({ _id: ObjectID(id)})
       .toArray()
       .then( (docs) => res.json(docs))
+  });
+
+  router.post('/', (req, res) => {
+    const newStats = req.body.stats;
+    highScoresCollection
+      .insertMany(newStats)
+      .then( () => {
+        highScoresCollection
+          .find()
+          .toArray()
+          .then( (docs) => res.json(docs))
+        });
+  });
+
+  router.delete('/all', (req, res) => {
+    highScoresCollection
+      .deleteMany({ })
+      .then( () => {
+        highScoresCollection
+          .find()
+          .toArray()
+          .then( (docs) => res.json(docs))
+        });
   });
 
   return router;
