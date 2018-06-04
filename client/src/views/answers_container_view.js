09fe7fad8aa43ef359f4d7db6c8a0abd79da7333
Andris <aundiks@gmail.com>
2018-06-04 15:47:54 +0100

    Fixed branch code

--- client/src/views/answers_container_view.js
+++ client/src/views/answers_container_view.js
@@ -1,25 +1,19 @@
 const PubSub = require('../helpers/pub_sub.js');
 const AnswerView = require('./answer_view.js');
 
-const AnswersContainerView = function(answers){
+const AnswersContainerView = function(answers, container){
   this.answers = answers;
   this.answerViews = [];
   this.container = container;
 }
 
-AnswersContainerView.prototype.createContainer = function(){
-  const container = document.createElement('div');
-  this.container = container;
-}
-
 AnswersContainerView.prototype.render = function () {
-  createContainer();
   this.answerViews = [];
   for (var i = 0; i < this.answers.length; i++) {
     const answerContainer = document.createElement('div');
     const answerView = new AnswerView(i, this.answers[i], answerContainer);
-    answerViews.render();
     this.container.appendChild(answerView);
+    answerView.render();
     this.answerViews.push(answerView);
   }
 };
