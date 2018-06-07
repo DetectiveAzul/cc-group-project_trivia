eab27e2a2fc88c187483ca4672f0f0e74f49780e
Jaime Lopez <detec.azul@gmail.com>
2018-06-07 12:26:12 +0100

    Added code to port listening on heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const parser = require('body-parser');
+let port = process.env.PORT || 8080;
 // const indexRouter = require('./routers/index_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
@@ -10,6 +11,6 @@ app.use(express.static(publicPath));
 app.use(parser.json());
 // app.use(indexRouter);
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });
